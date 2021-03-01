c166ab482c66a8d28c4964b8f01ead85a9e36ed8
David Jennings <djennings@esri.com>
2021-03-01 09:10:12 -0600

    Updated Http Command Line Help

--- src/main/java/com/esri/rttest/send/Http.java
+++ src/main/java/com/esri/rttest/send/Http.java
@@ -153,6 +153,8 @@ public class Http extends Send {
             System.err.println("desiredRatePerSec: Desired Rate. The tool will try to send at this rate if possible");
             System.err.println("numToSend: Number of lines to send");
             System.err.println("resueFile: true or false; if true the file is reused as needed to if numToSend is greater than number of lines in the file");
+            System.err.println("username or token: Defaults to empty string.  If token is provided leave password blank.");
+            System.err.println("password: Default to empty string.");
             System.err.println("");
         } else {
 
