f7d301f6007f566a8d0ed9436949c1e48d1550f4
David Jennings <djennings@esri.com>
2021-03-12 14:52:54 -0600

    Added another parameter to Mqtt to allow changing a epoch field to current system epoch

--- src/main/java/com/esri/rttest/send/Mqtt.java
+++ src/main/java/com/esri/rttest/send/Mqtt.java
@@ -33,6 +33,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONObject;
 
 /**
  *
@@ -48,6 +49,59 @@ public class Mqtt extends Send {
 
         while (linesIterator.hasNext()) {
             String line = linesIterator.next();
+
+            if (epochField != null ) {
+                Long epochVal = null;
+                // replace epoch
+                if (this.epochFieldDelimiter == '.') {
+                    // Starting from first label after .
+                    int jsonPathIndex = 1;
+
+                    // This will find assuming no Json Arrays in the path
+                    JSONObject json = new JSONObject(line);
+                    while (jsonPathIndex < epochJsonPathParts.length) {
+                        //System.out.println(jsonPathIndex + jsonPathParts[jsonPathIndex]);
+                        if (jsonPathIndex < epochJsonPathParts.length - 1) {
+                            // Every field before the last is an object
+                            json = json.getJSONObject(epochJsonPathParts[jsonPathIndex]);
+                        } else {
+                            // Last field convert to string
+                            epochVal = json.getLong(epochJsonPathParts[jsonPathIndex]);
+                            Long ts = System.currentTimeMillis();
+                            if (epochVal < 1000000000000L) {
+                                ts = (long) Math.round(ts / 1000.0);
+                            }
+                            json.put(epochJsonPathParts[jsonPathIndex], ts);
+                        }
+                        jsonPathIndex += 1;
+                    }
+                    //System.out.println(json.toString());
+                    line = json.toString();
+
+                } else {
+                    // Use +1 to make the index human friendly field 1 is first field instead of field 0 is first field
+                    String delim = this.epochFieldDelimiter.toString();
+                    String[] lineParts = line.split(delim);
+                    StringBuilder newLine = new StringBuilder();
+
+                    for (int i = 0; i < lineParts.length; i++) {
+                        if (i == this.epochFieldNumber - 1) {
+                            epochVal = Long.parseLong(line.split(delim)[this.epochFieldNumber - 1]);
+                            Long ts = System.currentTimeMillis();
+                            if (epochVal < 1000000000000L) {
+                                ts = (long) Math.round(ts / 1000.0);
+                            }
+                            newLine.append(ts.toString());
+                        } else {
+                            newLine.append(lineParts[i]);
+                        }
+                        if (i < lineParts.length - 1) newLine.append(delim);
+                    }
+                    //System.out.println(newLine);
+                    line = newLine.toString();
+                }
+            }
+
             MqttMessage message = new MqttMessage(line.getBytes());
             message.setQos(this.qos);
             try {
@@ -94,8 +148,13 @@ public class Mqtt extends Send {
     MqttClient mqttClient;
     String topic;
     Integer qos;
+    private String epochField = null;
+    private Character epochFieldDelimiter = null;
+    private Integer epochFieldNumber = null;
+    private String[] epochJsonPathParts = null;
 
-    public Mqtt(String host, String topic, String filename, Integer desiredRatePerSec, Long numToSend, String username, String password, boolean reuseFile, String clientId, Integer qos, String groupField, Integer groupRateSec) {
+    public Mqtt(String host, String topic, String filename, Integer desiredRatePerSec, Long numToSend, String username,
+                String password, boolean reuseFile, String clientId, Integer qos, String groupField, Integer groupRateSec, String epochField) {
 
         if (clientId == null) {
             UUID uuid = UUID.randomUUID();
@@ -109,6 +168,36 @@ public class Mqtt extends Send {
 
         this.topic = topic;
 
+        if (epochField != null) {
+            Long ts = (long) Math.round(System.currentTimeMillis() / 1000.0);
+
+            this.epochField = epochField;
+            // Parse the epochField
+            char[] epochFieldChars = epochField.toCharArray();
+            // Using toCharArray handled case where first char is \t (tab)
+
+            epochFieldDelimiter = Character.valueOf(epochFieldChars[0]);
+
+            if (epochFieldDelimiter == '.') {
+                // This means input it json file
+                epochJsonPathParts = epochField.split("\\.");
+                epochFieldNumber = null;
+            } else {
+                try {
+                    if (epochFieldDelimiter == 't') {
+                        // Trim (remove) tab and convert to Integer
+                        epochFieldDelimiter = '\t';
+                        epochFieldNumber = Integer.parseInt(epochField.substring(1));
+                    } else {
+                        // Drop first Char and convert to Integer
+                        epochFieldNumber = Integer.parseInt(epochField.substring(1));
+                    }
+                } catch (NumberFormatException e) {
+                    System.err.println("Failed to parse Integer from: " + epochField);
+                }
+                epochJsonPathParts = null;
+            }
+        }
         try {
             
             this.mqttClient = new MqttClient(host, clientId, null);
@@ -170,7 +259,7 @@ public class Mqtt extends Send {
             System.err.println("");
             System.err.println("host: mqtt host (e.g. tcp://mqtt.eclipse.org:1883");
             System.err.println("topic: Mqtt Topic");
-            System.err.println("file: file with lines of text to send to Elasticsearch; if folder then all files in the folder are sent one at a time alphabetically");
+            System.err.println("file: file with lines of text to send; if folder then all files in the folder are sent one at a time alphabetically");
             System.err.println("desiredRatePerSec|groupField: Use Number to specify a desiredRatePerSec|groupField start with delimiter followed by field number or period with json path to field");
             System.err.println("  groupField examples: ");
             System.err.println("    \",1\": comma delimited text field 1");
@@ -180,11 +269,13 @@ public class Mqtt extends Send {
             System.err.println("    \".properties.ts\": json data with field at path properties.ts");
             System.err.println("numToSend: Number of lines to send");
             System.err.println("username");
-            System.err.println("password");            
+            System.err.println("password");
             System.err.println("resueFile: true or false; if true the file is reused as needed to if numToSend is greater than number of lines in the file");
             System.err.println("groupRateSec: Send a group every groupRateSec seconds: default to 1");
             System.err.println("qos (Quality of Service): defaults to 2 exactly once");
             System.err.println("clientId: defaults to Random GUID");
+            System.err.println("epochField: defaults to null; Same format as groupField above. If specified epoch value is replaced with current system epoch");
+
             
             System.err.println("");
         } else {
@@ -198,6 +289,7 @@ public class Mqtt extends Send {
             String groupField = null;
             Integer groupRateSec = null;
             boolean reuseFile = true;
+            String epochField = null;
             
             String host = args[0];
             String topic = args[1];
@@ -236,6 +328,10 @@ public class Mqtt extends Send {
             if (numargs > 10) {
                 clientId = args[10];
             }
+
+            if (numargs > 11) {
+                epochField = args[11];
+            }
                                    
             System.out.println("host: " + host);
             System.out.println("topic: " + topic);
@@ -249,10 +345,11 @@ public class Mqtt extends Send {
             System.out.println("groupRateSec: " + groupRateSec);
             System.out.println("qos: " + qos);
             System.out.println("clientId: " + clientId);
+            System.out.println("epochField: " + epochField);
             System.out.println();
             
 
-            new Mqtt(host, topic, file, desiredRatePerSec, numToSend, username, password, reuseFile, clientId, qos, groupField, groupRateSec);
+            new Mqtt(host, topic, file, desiredRatePerSec, numToSend, username, password, reuseFile, clientId, qos, groupField, groupRateSec, epochField);
 
         }
     }
