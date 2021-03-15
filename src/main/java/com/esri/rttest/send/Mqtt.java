dd80d3d85779c3956a1c1791d7f05d32c5d6bf79
David Jennings <djennings@esri.com>
2021-03-15 10:14:41 -0500

    Trying commons cli

--- src/main/java/com/esri/rttest/send/Mqtt.java
+++ src/main/java/com/esri/rttest/send/Mqtt.java
@@ -29,6 +29,8 @@ import java.util.UUID;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
+
+import org.apache.commons.cli.*;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -153,7 +155,11 @@ public class Mqtt extends Send {
     private Integer epochFieldNumber = null;
     private String[] epochJsonPathParts = null;
 
-    public Mqtt(String host, String topic, String filename, Integer desiredRatePerSec, Long numToSend, String username,
+    public Mqtt() {
+
+    }
+
+    public void run(String host, String topic, String filename, Integer desiredRatePerSec, Long numToSend, String username,
                 String password, boolean reuseFile, String clientId, Integer qos, String groupField, Integer groupRateSec, String epochField) {
 
         if (clientId == null) {
@@ -251,107 +257,281 @@ public class Mqtt extends Send {
 
     }
 
+
+
     public static void main(String[] args) {
-        int numargs = args.length;
-        if (numargs < 5) {
-            System.err.println("Usage: Mqqt [host] [topic] [file] [desiredRatePerSec|groupField] [numToSend] (username=null) (password=null) (reuseFile=true) (groupRateSec) (qos=0) (clientId=randomGuid) \n");
-            System.err.println("java -cp target/rttest.jar com.esri.rttest.send.Mqtt tcp://52.191.131.159:1883 test /Users/davi5017/Downloads/safegraph_time_grouped.txt 100 10000 username password");
-            System.err.println("");
-            System.err.println("host: mqtt host (e.g. tcp://mqtt.eclipse.org:1883");
-            System.err.println("topic: Mqtt Topic");
-            System.err.println("file: file with lines of text to send; if folder then all files in the folder are sent one at a time alphabetically");
-            System.err.println("desiredRatePerSec|groupField: Use Number to specify a desiredRatePerSec|groupField start with delimiter followed by field number or period with json path to field");
-            System.err.println("  groupField examples: ");
-            System.err.println("    \",1\": comma delimited text field 1");
-            System.err.println("    \"|3\": pipe delimited text field 3 ");
-            System.err.println("    \"t2\": tab delimited text field 2 ");            
-            System.err.println("    \".ts\": json data with field at path ts");
-            System.err.println("    \".properties.ts\": json data with field at path properties.ts");
-            System.err.println("numToSend: Number of lines to send");
-            System.err.println("username");
-            System.err.println("password");
-            System.err.println("resueFile: true or false; if true the file is reused as needed to if numToSend is greater than number of lines in the file");
-            System.err.println("groupRateSec: Send a group every groupRateSec seconds: default to 1");
-            System.err.println("qos (Quality of Service): defaults to 2 exactly once");
-            System.err.println("clientId: defaults to Random GUID");
-            System.err.println("epochField: defaults to null; Same format as groupField above. If specified epoch value is replaced with current system epoch");
 
-            
-            System.err.println("");
-        } else {
+        Mqtt app = new Mqtt();
+        String appName = app.getClass().getSimpleName();
 
-            Integer desiredRatePerSec = -1;
-            
-            String clientId = null;
-            Integer qos = null;
-            String username = null;
-            String password = null;
-            String groupField = null;
-            Integer groupRateSec = null;
-            boolean reuseFile = true;
-            String epochField = null;
-            
-            String host = args[0];
-            String topic = args[1];
-            String file = args[2];
-            
+        Options options = new Options();
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.setWidth(160);
+        formatter.setLeftPadding(1);
+
+        Option helpOp = Option.builder("help")
+                .longOpt("help")
+                .desc("display help and exit")
+                .build();
+
+        Option hostOp = Option.builder("h")
+                .longOpt("host")
+                .required()
+                .hasArg()
+                .desc("[Required] Mqtt host (e.g. tcp://mqtt.eclipse.org:1883")
+                .build();
+
+        Option topicOp = Option.builder("t")
+                .longOpt("topic")
+                .required()
+                .hasArg()
+                .desc("[Required] Mqtt Topic")
+                .build();
+
+        Option fileOp = Option.builder("f")
+                .longOpt("file")
+                .required()
+                .hasArg()
+                .desc("[Required] File with lines of text to send; if a folder is specified all files in the folder are sent one at a time alphabetically")
+                .build();
+
+        Option rateOp = Option.builder("r")
+                .longOpt("rate")
+                .required()
+                .hasArg()
+                .desc("[Required] desiredRatePerSec or groupField\n" +
+                        "- desiredRatePerSec: Integer value \n" +
+                        "- groupField: String value specified as delimiterFieldNumber or JsonPath (Examples):\n" +
+                        "-> \",1\": comma delimited text field 1\n" +
+                        "-> \"|3\": pipe delimited text field 3\n" +
+                        "-> \"t2\": tab delimited text field 2\n" +
+                        "-> \".ts\": json data with field at path ts\n" +
+                        "-> \".prop.ts\": json data field at path prop.ts")
+                .build();
+
+        Option numToSendOp = Option.builder("n")
+                .longOpt("number-to-send")
+                .required()
+                .hasArg()
+                .desc("[Required] Number of lines to send")
+                .build();
+
+        Option usernameOp = Option.builder("u")
+                .longOpt("username")
+                .hasArg()
+                .desc("Mqtt Server Username; default no username")
+                .build();
+
+        Option passwordOp = Option.builder("p")
+                .longOpt("password")
+                .hasArg()
+                .desc("Mqtt Server Password; default no password")
+                .build();
+
+        Option onetimeOp = Option.builder("o")
+                .longOpt("one-time")
+                .desc("Send lines only one time. Stop when all lines have been sent.")
+                .build();
+
+        Option groupRateSecOp = Option.builder("g")
+                .longOpt("group-rate-sec")
+                .hasArg()
+                .desc("Number seconds between each time a group of lines with same groupField are sent; defaults to 1")
+                .build();
+
+        Option qosOp = Option.builder("q")
+                .longOpt("qos")
+                .hasArg()
+                .desc("Quality of Service: defaults to 2 => exactly once")
+                .build();
+
+        Option clientIdOp = Option.builder("c")
+                .longOpt("client-id")
+                .hasArg()
+                .desc("Client ID; default to random guid")
+                .build();
+
+        Option epochFieldOp = Option.builder("e")
+                .longOpt("epoch-field")
+                .hasArg()
+                .desc("Replace specified epoch field with current epoch; specified in same way as groupField; default no epoch-field")
+                .build();
+
+        options.addOption(helpOp);
+        options.addOption(hostOp);
+        options.addOption(topicOp);
+        options.addOption(fileOp);
+        options.addOption(rateOp);
+        options.addOption(numToSendOp);
+        options.addOption(usernameOp);
+        options.addOption(passwordOp);
+        options.addOption(onetimeOp);
+        options.addOption(groupRateSecOp);
+        options.addOption(qosOp);
+        options.addOption(clientIdOp);
+        options.addOption(epochFieldOp);
+
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            System.err.println();
+            formatter.printHelp(appName, options);
+            System.exit(1);
+        }
+
+        if (cmd.hasOption("--help")) {
+            System.out.println("Send lines from a file to an Mqtt Server");
+            System.out.println("");
+            formatter.printHelp(appName, options);
+            System.exit(0);
+        }
+
+        String host = null;
+        if(cmd.hasOption("h")) {
+            host = cmd.getOptionValue("h");
+        }
+        System.out.println("host: " + host);
+
+
+        String topic = null;
+        if(cmd.hasOption("t")) {
+            topic = cmd.getOptionValue("t");
+        }
+        System.out.println("topic: " + topic);
+
+
+        String file = null;
+        if(cmd.hasOption("f")) {
+            file = cmd.getOptionValue("f");
+        }
+        System.out.println("file: " + file);
+
+
+        Integer desiredRatePerSec = null;
+        String groupField = null;
+        if(cmd.hasOption("r")) {
+            String rateGroupVal = cmd.getOptionValue("r");
             try {
                 // If integer assume it's a desired rate
-                desiredRatePerSec = Integer.parseInt(args[3]);
+                desiredRatePerSec = Integer.parseInt(rateGroupVal);
             } catch (NumberFormatException e ) {
                 // Not a number assume group Field
-                groupField = args[3];
+                groupField = rateGroupVal;
             }
+        }
+        System.out.println("desiredRatePerSec: " + desiredRatePerSec);
+        System.out.println("groupField: " + groupField);
 
-            Long numToSend = Long.parseLong(args[4]);
+        Long numToSend = null;
+        if(cmd.hasOption("n")) {
+            try {
+                numToSend = Long.parseLong(cmd.getOptionValue("n"));
+            } catch (NumberFormatException e) {
+                System.out.println("");
+                System.out.println("Invalid value for num-to-send (n). Must be an Integer");
+                System.out.println("");
+                formatter.printHelp(appName, options);
+                System.exit(1);
+            }
+        }
+        System.out.println("numToSend: " + numToSend);
 
-            if (numargs > 5) {
-                username = args[5];
-            }
 
-            if (numargs > 6) {
-                password = args[6];
-            }
+        String username = null;
+        if(cmd.hasOption("u")) {
+            username = cmd.getOptionValue("u");
+        }
+        System.out.println("username: " + username);
 
-            if (numargs > 7) {
-                reuseFile = Boolean.parseBoolean(args[7]);
-            }
 
-            if (numargs > 8) {
-                groupRateSec = Integer.parseInt(args[8]);
-            }
-            
-            if (numargs > 9) {
-                qos = Integer.parseInt(args[9]);
-            }
-            
-            if (numargs > 10) {
-                clientId = args[10];
-            }
+        String password = null;
+        if(cmd.hasOption("p")) {
+            password = cmd.getOptionValue("p");
+        }
+        System.out.println("password: " + password);
 
-            if (numargs > 11) {
-                epochField = args[11];
-            }
-                                   
-            System.out.println("host: " + host);
-            System.out.println("topic: " + topic);
-            System.out.println("file: " + file);
-            System.out.println("desiredRatePerSec: " + desiredRatePerSec);
-            System.out.println("groupField: " + groupField);
-            System.out.println("numToSend: " + numToSend);
-            System.out.println("username: " + username);
-            System.out.println("password: " + password);
-            System.out.println("reuseFile: " + reuseFile);
-            System.out.println("groupRateSec: " + groupRateSec);
-            System.out.println("qos: " + qos);
-            System.out.println("clientId: " + clientId);
-            System.out.println("epochField: " + epochField);
-            System.out.println();
-            
+        boolean reuseFile = true;
+        if(cmd.hasOption("o")) {
+            reuseFile = false;
+        }
+        System.out.println("reuseFile : " + reuseFile);
 
-            new Mqtt(host, topic, file, desiredRatePerSec, numToSend, username, password, reuseFile, clientId, qos, groupField, groupRateSec, epochField);
+        Integer groupRateSec = null;
+        if(cmd.hasOption("g")) {
+            try {
+                groupRateSec = Integer.parseInt(cmd.getOptionValue("g"));
+                if (groupRateSec < 0) {
+                    throw new Exception("Must be positive");
+                }
+                if (groupField == null ) {
+                    throw new Exception("Must also specify a groupField");
+                }
+            } catch (NumberFormatException ne) {
+                System.out.println("");
+                System.out.println("Invalid value for group-rate-sec (g).  Must be an Integer");
+                System.out.println("");
+                formatter.printHelp(appName, options);
+                System.exit(1);
+            } catch (Exception e ) {
+                System.out.println("");
+                System.out.println("Invalid value for group-rate-sec (g). " + e.getMessage());
+                System.out.println("");
+                formatter.printHelp(appName, options);
+                System.exit(1);
+            }
 
         }
+        if (groupField != null) {
+            // default to 1
+            groupRateSec = 1;
+        }
+        System.out.println("groupRateSec: " + groupRateSec);
+
+        Integer qos = null;
+        if(cmd.hasOption("g")) {
+            try {
+                qos = Integer.parseInt(cmd.getOptionValue("q"));
+                if (qos < 0 || qos > 3) {
+                    throw new Exception("Must be 0, 1 or 2");
+                }
+            } catch (NumberFormatException ne) {
+                System.out.println("");
+                System.out.println("Invalid value for qos (q).  Must be an Integer");
+                System.out.println("");
+                formatter.printHelp(appName, options);
+                System.exit(1);
+            } catch (Exception e) {
+                System.out.println("");
+                System.out.println("Invalid value for qos (q) " + e.getMessage());
+                System.out.println("");
+                formatter.printHelp(appName, options);
+                System.exit(1);
+            }
+        }
+        if (qos == null) {
+            qos = 2;
+        }
+        System.out.println("qos: " + qos);
+
+
+        String clientId = null;
+        if(cmd.hasOption("c")) {
+            clientId = cmd.getOptionValue("c");
+        }
+        System.out.println("clientId : " + clientId);
+
+        String epochField = null;
+        if(cmd.hasOption("e")) {
+            epochField = cmd.getOptionValue("e");
+        }
+        System.out.println("epochField : " + epochField);
+
+        app.run(host, topic, file, desiredRatePerSec, numToSend, username, password, reuseFile, clientId, qos, groupField, groupRateSec, epochField);
     }
 
 }
